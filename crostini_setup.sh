ad2bf480a73a197e8cfe75537f5810e3ef0d2619
Graham Gilbert <graham@grahangilbert.com>
2018-11-28 19:36:55 +0000

    Python and docker

--- crostini_setup.sh
+++ crostini_setup.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 apt update -y
 apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+   apt-transport-https \
+   ca-certificates \
+   curl \
+   software-properties-common \
+   make \
+   build-essential \
+   libssl-dev \
+   zlib1g-dev \
+   libbz2-dev \
+   libreadline-dev \
+   libsqlite3-dev \
+   wget \
+   llvm \
+   libncurses5-dev \
+   libncursesw5-dev \
+   xz-utils \
+   tk-dev \
+   python
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+# Python 3.7
+ wget https://www.python.org/ftp/python/3.7.1/Python-3.7.1.tgz
+ tar xvf Python-3.7.1.tgz
+ cd Python-3.7.1
+ ./configure --enable-optimizations
+ make -j8
+ make altinstall
+
+ rm -rf Python-3.7.1*
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 
@@ -36,5 +60,7 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
 apt update -y
-apt install -y docker-ce
+git clone git@github.com:abiosoft/crostini-docker.git /tmp/crostini-docker
+cd /tmp/crostini-docker
+./install.sh
 # if ssh key isn't here exit and tell person to copy it to place
